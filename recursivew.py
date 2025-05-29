83319870fb3b07b084ed0b5d38173d0985492a6d
Kenneth Batson <kenneth.batson@microchip.com>
2025-05-29 09:40:13 -0700

    [250529] - Updated script with menu selections, more robust error handling

--- recursivew.py
+++ recursivew.py
@@ -1,32 +1,59 @@
 
 def duration():
-    w = input("Input initial weight: ")
-    w = float(w)
-    d = input("Input loss duration: ")
+    while True:
+        try:
+            w = input("Input initial weight: ")
+            w = float(w)
+            d = input("Input loss duration: ")
+            d = int(d)
+            per = input("Input percentage weight loss (between 0 and 1): ")
+            per = float(per)
+            if(per > 1):
+                print("WARNING!! You have selected a weight loss percentage above 1%. This is NOT recommended!")
+        except ValueError:
+            print("One or more of your inputs is not valid, please try again.")
+        else:
+            break
     print(w)
     c = 0
     for i in range (int(d)):
-        p = w*.01
+        p = w*(per/100)
         w = w - p
-        print("Week: " + str(i+1+(c*8)) + " Weight: " +"{:.2f}".format(w))
+        print("Week: " + str(i+1+(c*8)) + " Weight: " +"{:.2f}".format(w)+ " Weekly Loss: " + "{:.2f}".format(p) + " Daily Loss: " + "{:.2f}".format(p/7))
         if (i+1)%12 == 0:
             c = c + 1
             print("Maintenance Period: + 8 Weeks")
     menu()
     
 def goal():
-    w = input("Input initial weight: ")
-    w = float(w)
-    g = input("Input goal weight: ")
-    g = int(g)
+    while True:
+        try:
+            w = input("Input initial weight: ")
+            w = float(w)
+            per = input("Input percentage weight loss (between 0 and 1): ")
+            per = float(per)
+            if(per > 1):
+                print("WARNING!! You have selected a weight loss percentage above 1%. This is NOT recommended!")
+            g = input("Input goal weight: ")
+            g = int(g)
+        except ValueError:
+            print("One or more of your inputs is not valid, please try again.")
+        else:
+            break
 
     i = 0
+    c = 0
     while w > g:
-        p = w*.01
+        p = w*(per/100)
         w = w - p
         i = i + 1
-
+        
+        if (i+1)%12 == 0:
+            c = c + 1
     print("Weeks to target: " + str(i))
+    print("Weeks of maintenance: " + str(c*8))
+    print("Total time: " + str((c*8)+i))
+    
     menu()
 
 def menu():
@@ -38,4 +65,7 @@ def menu():
             goal()
         case "e":
             return
+        case _:
+            print("Invalid choice!")
+            menu()
 menu()
